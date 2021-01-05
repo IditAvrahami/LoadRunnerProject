5287e7470e5289e0cb2286a41189649095712c99
Idit Avrahami <iditav@std.hac.ac.il>
2021-01-05 09:57:58 +0200

    first idit

--- include/Enemy.h
+++ include/Enemy.h
@@ -1,1 +1,22 @@
-#pragma once#include <SFML/Graphics.hpp>#include "MovingObject.h"class Enemy : public MovingObject{public:    Enemy(sf::Texture picture, const int speed);    virtual ~Enemy() = default;    virtual void setLocation(const int y, const int x);    virtual void print(sf::RenderWindow& window);    virtual void move(); // empy    virtual sf::Sprite getSprite();   virtual sf::Vector2f getLocation()const;    //virtual bool checkNextMove(const int direction);    virtual void setSpeed(const int);    virtual int getSpeed();private:    sf::Sprite m_enemyPng;    int m_speed;};
+#pragma once
+#include <SFML/Graphics.hpp>
+#include "MovingObject.h"
+
+class Enemy : public MovingObject
+{
+public:
+    Enemy(sf::Texture picture, const int speed);
+    virtual ~Enemy() = default;
+    virtual void setLocation(const int y, const int x);
+    virtual void print(sf::RenderWindow& window);
+    virtual void move(); // empy
+    virtual sf::Sprite getSprite();
+   virtual sf::Vector2f getLocation()const;
+    //virtual bool checkNextMove(const int direction);
+    virtual void setSpeed(const int);
+    virtual int getSpeed();
+
+private:
+    sf::Sprite m_enemyPng;
+    int m_speed;
+};
