09ce7d5e29ba7aae24d6ee43907e06eb161dbc81
Daniel Abayev <danielaba@std.hac.ac.il>
2021-01-05 19:24:32 +0200

    player for today

--- include/Player.h
+++ include/Player.h
@@ -6,15 +6,17 @@ class Player : public MovingObject
 {
 public:
     Player(sf::Texture picture, const int speed);
+    Player(sf::Texture picture);
     virtual ~Player() =default;
     virtual void setLocation(const int y, const int x);
-    virtual void print();
-    virtual void move();
+    virtual void print(sf::RenderWindow& window);
+    virtual void move(sf::Time &timePassed);
     void dig();
     bool canDig();
-    virtual sf::Sprite getSprite() ;
-    virtual bool checkNextMove();
+    //virtual sf::Sprite getSprite() ;
+    //virtual bool checkNextMove();
     virtual void setDirection(sf::Keyboard::Key);
+    void changeface(bool toRight);
 
     //virtual void setLocation();
 
